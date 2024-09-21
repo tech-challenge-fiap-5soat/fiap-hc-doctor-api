fb292e9c779caa65986c95f8f33642eb8942264c
Reinaldo Augusto Bocchi <reinaldo.bocchi@gmail.com>
2024-09-21 14:04:45 -0300

    feat: initial commit

---
+++ src/test/java/com/fiap/hc/doctor_api/DoctorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.hc.doctor_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
