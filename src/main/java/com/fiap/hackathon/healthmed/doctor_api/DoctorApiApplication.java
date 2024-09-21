8c0068bc6f06b35e4c32ed9b21222d7a6bc4444e
Reinaldo Augusto Bocchi <reinaldo.bocchi@gmail.com>
2024-09-21 14:11:42 -0300

    feat: refactor package name

--- src/main/java/com/fiap/hackathon/healthmed/doctor_api/DoctorApiApplication.java
+++ src/main/java/com/fiap/hackathon/healthmed/doctor_api/DoctorApiApplication.java
@@ -1,4 +1,4 @@
-package com.fiap.hc.doctor_api;
+package com.fiap.hackathon.healthmed.doctor_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
