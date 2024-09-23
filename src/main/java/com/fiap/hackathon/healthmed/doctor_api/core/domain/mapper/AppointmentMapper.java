42ef1898643499186b66199fd887557abf03e19e
Reinaldo Augusto Bocchi <reinaldo.bocchi@gmail.com>
2024-09-23 20:52:01 -0300

    chore: add call to scheduling api to create doctor appointments when doctor create an set of appointments

--- src/main/java/com/fiap/hackathon/healthmed/doctor_api/core/domain/mapper/AppointmentMapper.java
+++ src/main/java/com/fiap/hackathon/healthmed/doctor_api/core/domain/mapper/AppointmentMapper.java
@@ -1,6 +1,7 @@
 package com.fiap.hackathon.healthmed.doctor_api.core.domain.mapper;
 
 import com.fiap.hackathon.healthmed.doctor_api.core.domain.model.Appointment;
+import com.fiap.hackathon.healthmed.doctor_api.dataprovider.gateway.request.CreateDoctorScheduleRequest;
 import com.fiap.hackathon.healthmed.doctor_api.entrypoint.controller.payload.request.AppointmentCreateRequest;
 import com.fiap.hackathon.healthmed.doctor_api.entrypoint.controller.payload.response.AppointmentResponse;
 import org.mapstruct.Mapper;
@@ -22,6 +23,16 @@ public abstract class AppointmentMapper {
     public abstract AppointmentResponse appointmentToAppointmentResponse(Appointment appointment);
 
     public abstract List<AppointmentResponse> appointmentListToAppointmentResponseList(List<Appointment> appointments);
+
+    public abstract List<Appointment> appointmentCreateRequestListToAppointmentList(List<AppointmentCreateRequest> request);
+
+    @Mapping(target = "startTime", expression = "java(dateAndTimeToLocalDateTime(appointment.getDate(), appointment.getStartTime()))")
+    @Mapping(target = "endTime", expression = "java(dateAndTimeToLocalDateTime(appointment.getDate(), appointment.getEndTime()))")
+    @Mapping(target = "date", expression = "java(dateToLocalDateTime(appointment.getDate()))")
+    @Mapping(target = "doctorId", source = "appointment.doctor.id")
+    public abstract CreateDoctorScheduleRequest appointmentToCreateDoctorScheduleRequest(Appointment appointment);
+
+    public abstract List<CreateDoctorScheduleRequest> appointmentListToCreateDoctorScheduleRequestList(List<Appointment> appointments);
 
     protected LocalDateTime dateAndTimeToLocalDateTime(LocalDate date, LocalTime time) {
         if (date == null || time == null) {
--- src/main/java/com/fiap/hackathon/healthmed/doctor_api/core/service/impl/AppointmentServiceImpl.java
+++ src/main/java/com/fiap/hackathon/healthmed/doctor_api/core/service/impl/AppointmentServiceImpl.java
@@ -6,9 +6,11 @@ import com.fiap.hackathon.healthmed.doctor_api.core.domain.model.Appointment;
 import com.fiap.hackathon.healthmed.doctor_api.core.domain.model.Doctor;
 import com.fiap.hackathon.healthmed.doctor_api.core.service.AppointmentService;
 import com.fiap.hackathon.healthmed.doctor_api.core.service.DoctorService;
+import com.fiap.hackathon.healthmed.doctor_api.dataprovider.gateway.SchedulingApiClient;
+import com.fiap.hackathon.healthmed.doctor_api.dataprovider.gateway.request.CreateDoctorScheduleRequest;
 import com.fiap.hackathon.healthmed.doctor_api.dataprovider.repository.AppointmentRepository;
-import com.fiap.hackathon.healthmed.doctor_api.entrypoint.controller.payload.request.AppointmentCreateRequest;
 import com.fiap.hackathon.healthmed.doctor_api.entrypoint.controller.payload.request.AppointmentUpdateRequest;
+import com.fiap.hackathon.healthmed.doctor_api.entrypoint.controller.payload.request.DoctorAppointmentCreateRequest;
 import com.fiap.hackathon.healthmed.doctor_api.entrypoint.controller.payload.response.AppointmentResponse;
 import lombok.RequiredArgsConstructor;
 import org.mapstruct.factory.Mappers;
@@ -28,22 +30,31 @@ public class AppointmentServiceImpl implements AppointmentService {
     private final AppointmentMapper mapper = Mappers.getMapper(AppointmentMapper.class);
 
     private final DoctorService doctorService;
+    private final SchedulingApiClient schedulingApiClient;
     private static final String MESSAGE_DOCTOR_NOT_FOUND = "doctor not found";
     private static final String MESSAGE_APPOINTMENT_NOT_FOUND = "appointment not found";
     @Override
-    public AppointmentResponse createAppointment(AppointmentCreateRequest request) {
+    public void createAppointment(DoctorAppointmentCreateRequest request) {
         Optional<Doctor> doctorOp = doctorService.findDoctorById(request.doctorId());
 
         if (doctorOp.isEmpty()) {
             throw new BusinessException(MESSAGE_DOCTOR_NOT_FOUND);
         }
 
-        Appointment appointment = mapper.appointmentCreateRequestToAppointment(request);
-        appointment.setDoctor(doctorOp.get());
+        List<Appointment> appointments = mapper.appointmentCreateRequestListToAppointmentList(request.appointments());
 
-        Appointment saved = repository.save(appointment);
+        appointments.forEach(a -> {
+            a.setDoctor(doctorOp.get());
+        });
 
-        return mapper.appointmentToAppointmentResponse(saved);
+        List<Appointment> saved = repository.saveAll(appointments);
+        sendAppointmentsToSchedulingApi(doctorOp.get().getId(), saved);
+    }
+
+    private void sendAppointmentsToSchedulingApi(UUID doctorId, List<Appointment> appointments) {
+        List<CreateDoctorScheduleRequest> request = mapper.appointmentListToCreateDoctorScheduleRequestList(appointments);
+
+        schedulingApiClient.createDoctor(doctorId, request);
     }
 
     @Override
