a1cb53e28a43254f7ea699ea5aaf47bd14bac535
Reinaldo Augusto Bocchi <reinaldo.bocchi@gmail.com>
2024-09-27 11:10:55 -0300

    chore add tests libs

--- src/test/java/com/fiap/hc/doctor_api/DoctorApiApplicationTests.java
+++ src/test/java/com/fiap/hc/doctor_api/DoctorApiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.fiap.hc.doctor_api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DoctorApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
